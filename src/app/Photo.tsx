5533b6e51ea2d7bfd38350d257a8ec6ced31f6b1
euouae682 <jasonuzhao682@gmail.com>
2024-02-04 21:36:24 -0800

    final changes

--- src/app/Photo.tsx
+++ src/app/Photo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Photo(props: {url: string, desc: string, caption: string, align: string}) {
+export default function Photo(props: {url: string, desc: string[], caption: string, align: string}) {
     return (
         <div className={`flex mx-20 my-80 ${props.align === 'left' ? 'flex-row' : 'flex-row-reverse'}`}>
             <div className={`w-1/2 m-5`}>
@@ -10,7 +10,9 @@ export default function Photo(props: {url: string, desc: string, caption: string
                 </div>
             </div>
             <div className="w-1/2 m-5 text-2xl">
-                { props.desc }
+                { props.desc.map((paragraph) => {
+                    return <p key={paragraph} className="pb-5">{ paragraph }</p>
+                }) }
             </div>
         </div>
     )
